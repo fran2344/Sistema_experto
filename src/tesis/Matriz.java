c26ac3bb96a22bfdc4f23676e75299297c85f0d6
fran2344 <ardonfrancisco456@gmail.com>
2016-05-27 22:34:58 -0600

    sin problema de creacion de perfiles

--- src/tesis/Matriz.java
+++ src/tesis/Matriz.java
@@ -9,11 +9,11 @@ public class Matriz implements java.io.Serializable{
     public String posicion_g="";
 //bandera nos indica en que dia del mes estamos es decir
 //primer lunes del mes o segundo o tercero
-    
+
     public Matriz(){
-        
+
     }
-    
+
     public String consulta(String Hora,String P,int bandera){ //aqui deberia ir tambien el parametro perfil (hora y posición)
         String info="";
         String[]hora_minuto=Hora.split(":");
@@ -57,22 +57,24 @@ public class Matriz implements java.io.Serializable{
                         posicion_g=P;
                         info=analizar(temp1,temp2,temp3);
                         //segunda premisa
-                        
-                        
+
+
                         //tercera premisa
-                        
-                        
-                        
+
+
+
                         //cuarta premisa
                 }
             }else{
                 //no se hace nada porque no ha configurado un perfil para ese dia
                 //podriamos hacer una sugerencia
             }
+            if(info.equals(""))
+                info="null";
         return info;
     }
-    
-    
+
+
     public int convertir_minuto(int minuto){ //devuelbe un minuto como puntero de la matriz
         int minute=0;
         if(minute<=20){
@@ -84,22 +86,22 @@ public class Matriz implements java.io.Serializable{
         }
         return minute;
     }
-    
+
     public void ingresar(String Hora,String P,String perfil){
         Nodo nuevo=new Nodo(P,perfil);
-        
+
         String[]hora_minuto=Hora.split(":");
         int Hours=Integer.parseInt(hora_minuto[0]);
         int Minute=Integer.parseInt(hora_minuto[1]);
         int H=convertir_minuto(Minute);
-        H=Hours+H;        
-        
+        H=Hours+H;
+
         for(int i=H;i<72;i++){
             matrix[i][bandera]=nuevo;
         }
     }
-    
-    public String analizar(Nodo temp1,Nodo temp2, Nodo temp3){ 
+
+    public String analizar(Nodo temp1,Nodo temp2, Nodo temp3){
     // analizamos si alguno de los 3 esta vacio se manda a analisis 2
         String info="";
         if(temp1==null){
@@ -130,11 +132,11 @@ public class Matriz implements java.io.Serializable{
                     return info3; // si contiene un perfil se retorna
                 }else if((info1.equals(""))&&(info2.equals(""))&&(info3.equals(""))){
                     return info; // si los 3 estan vacios se devuelbe una cadena vacia indica que no habra accion
-                    
+
                 }
                 // si alguno tiene un comentario se devuelbe el comentario mas cercano
                 else if(info1.equals("#")){
-                    return info1; 
+                    return info1;
                 }else if(info2.equals("#")){
                     return info2;
                 }else if(info3.equals("#")){
@@ -144,7 +146,7 @@ public class Matriz implements java.io.Serializable{
         }
         return info;
     }
-    
+
     public String analizar2(Nodo temp1, Nodo temp2){
         String info="";
         if(temp1==null){ //si el primer nodo esta vacio revisamos si el segundo tambien
@@ -164,7 +166,7 @@ public class Matriz implements java.io.Serializable{
                 //ambos tienen la posicion correcta y perfil igual
                 if(temp1.perfil==temp2.perfil){
                     info="$"+temp1.perfil; // se envia el perfil de cualquiera de los 2
-                //ambos tienen la posicion correcta y perfil distinto    
+                //ambos tienen la posicion correcta y perfil distinto
                 }else{
                     info="$"+temp1.perfil; //tomo el perfil del dia mas cercano
                 }
@@ -177,7 +179,7 @@ public class Matriz implements java.io.Serializable{
         }
         return info;
     }
-    
+
     public void escribir(String fichero) throws IOException, ClassNotFoundException{ //metodo para serializar la matriz
       //
         FileOutputStream fos = new FileOutputStream(fichero);
@@ -186,7 +188,7 @@ public class Matriz implements java.io.Serializable{
         fos.close();
       //
     }
-    
+
     public void leer(String fichero) throws IOException, ClassNotFoundException{
         Nodo [][]matrix2=new Nodo[72][4];
         FileInputStream fis = new FileInputStream(fichero);
--- src/tesis/tesis.java
+++ src/tesis/tesis.java
@@ -36,7 +36,7 @@ public class tesis {
         mat.escribir(sFichero);
         return prueba;
     }
-    
+
     @WebMethod(operationName = "ingresar_perfil")
     public String ingresar_perfil(String id_phone,String hora,String posicion,int dia_del_mes,String perfil,int dia) throws IOException, ClassNotFoundException {
         String sFichero=id_phone+"_"+dia+".dat"; //se busca el fichero del usuario
@@ -53,27 +53,27 @@ public class tesis {
             prueba="fichero no existía";
         }
         //ya tengo un documento estable por cada telefono
-        
-        
+
+
         ///aqui registramos el perfil en la bd
         prueba=set_perfil(id_phone, perfil);
         ////
-        
+
         mat.bandera=dia_del_mes;
         mat.ingresar(hora, posicion, perfil);
         mat.escribir(sFichero);
         return prueba;
     }
-    
+
     @WebMethod(operationName = "registro")
     public String registro(String correo, String pass, String nombre) {
-        /////////////// 
+        ///////////////
         Connection c = null;
         Statement s = null;
         try{
             Class.forName("org.postgresql.Driver");
-//          c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Perfiles","postgres","ardfra5a");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","admin");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/profile","postgres","admin");
+//            c = DriverManager.getConnection("jdbc:postgresql://ec2-54-191-108-207.us-west-2.compute.amazonaws.com:5432/profile","postgres","admin");
             c.setAutoCommit(false);
             s = c.createStatement();
             String consulta="insert into usuario(mail,pass,nombre)values ('"+correo+"','"+pass+"','"+nombre+"');";
@@ -83,25 +83,25 @@ public class tesis {
             c.close();
             String resultado = get_id(correo, pass);
             return resultado;
-            
+
         }catch(Exception e){
             System.out.println(e);
             return "#Error";
-        }        
-        //////////////        
+        }
+        //////////////
     }
-    
-    
-    
+
+
+
     @WebMethod(operationName = "get_id")
     public String get_id(String correo, String pass) {
-        /////////////// 
+        ///////////////
         Connection c = null;
         Statement s = null;
         try{
             Class.forName("org.postgresql.Driver");
-//            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Perfiles","postgres","ardfra5a");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","admin");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/profile","postgres","admin");
+//            c = DriverManager.getConnection("jdbc:postgresql://ec2-54-191-108-207.us-west-2.compute.amazonaws.com:5432/profile","postgres","admin");
             c.setAutoCommit(false);
             s = c.createStatement();
             ResultSet rs = s.executeQuery("select id_user from usuario where mail='"+correo+"' and pass='"+pass+"';");
@@ -114,23 +114,23 @@ public class tesis {
             s.close();
             c.close();
             return resultado;
-            
+
         }catch(Exception e){
             return "error";
-        }        
-        //////////////        
+        }
+        //////////////
     }
-    
-    
+
+
     @WebMethod(operationName = "set_perfil")
     public String set_perfil(String id_usuario,String perfil) {
-        /////////////// 
+        ///////////////
         Connection c = null;
         Statement s = null;
         try{
             Class.forName("org.postgresql.Driver");
-//          c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Perfiles","postgres","ardfra5a");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","admin");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/profile","postgres","admin");
+//            c = DriverManager.getConnection("jdbc:postgresql://ec2-54-191-108-207.us-west-2.compute.amazonaws.com:5432/profile","postgres","admin");
             c.setAutoCommit(false);
             s = c.createStatement();
             s.executeUpdate("insert into perfil(id_usuario,perfil)"
@@ -140,23 +140,23 @@ public class tesis {
             c.commit();
             c.close();
             return resultado;
-            
+
         }catch(Exception e){
             return "false";
-        }        
-        //////////////        
+        }
+        //////////////
     }
-    
-    
+
+
     @WebMethod(operationName = "get_perfiles")
     public String get_perfiles(String id_usuario) {
-        /////////////// 
+        ///////////////
         Connection c = null;
         Statement s = null;
         try{
             Class.forName("org.postgresql.Driver");
-//          c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Perfiles","postgres","ardfra5a");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","admin");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/profile","postgres","admin");
+//            c = DriverManager.getConnection("jdbc:postgresql://ec2-54-191-108-207.us-west-2.compute.amazonaws.com:5432/profile","postgres","admin");
             c.setAutoCommit(false);
             s = c.createStatement();
             ResultSet rs = s.executeQuery("select distinct perfil from perfil where id_usuario="+id_usuario+";");
@@ -169,11 +169,11 @@ public class tesis {
             s.close();
             c.close();
             return resultado;
-            
+
         }catch(Exception e){
             return "#Error";
-        }        
-        //////////////        
+        }
+        //////////////
     }
-    
+
 }
